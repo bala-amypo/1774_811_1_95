98bd6552643937cd24cd3df38576fdf62fca2362
bala-amypo <bala@amypo.in>
2025-12-21 17:38:12 +0000

    User commit

--- src/main/java/com/example/demo/dto/RegisterRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-public class RegisterRequest {
-    private String name;
-    private String email;
-    private String password;
-
-    // getters & setters
-}
--- src/main/java/com/example/demo/repository/BudgetPlanRepository.java
+++ src/main/java/com/example/demo/repository/BudgetPlanRepository.java
@@ -1,6 +1,15 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.BudgetPlan;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface BudgetPlanRepository extends JpaRepository<BudgetPlan, Long> {}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.BudgetPlan;
+import com.example.demo.model.User;
+
+@Repository
+public interface BudgetPlanRepository extends JpaRepository<BudgetPlan, Long> {
+
+    Optional<BudgetPlan> findByUserAndMonthAndYear(User user, Integer month, Integer year);
+}
