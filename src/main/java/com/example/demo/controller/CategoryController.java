66b27ba79e9e121e3cfdfb38aeeee797d8eaca4a
bala-amypo <bala@amypo.in>
2025-12-21 18:31:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -2,11 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.Category;
 import com.example.demo.repository.CategoryRepository;
@@ -21,12 +17,12 @@ public class CategoryController {
         this.repository = repository;
     }
 
-    @PostMapping
+    @PostMapping("/create")
     public Category create(@RequestBody Category category) {
         return repository.save(category);
     }
 
-    @GetMapping
+    @GetMapping("/all")
     public List<Category> getAll() {
         return repository.findAll();
     }
