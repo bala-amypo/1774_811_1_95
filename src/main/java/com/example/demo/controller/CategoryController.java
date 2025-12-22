8d29657a8948eafbff53a9adf58f829dc58865c1
bala-amypo <bala@amypo.in>
2025-12-22 04:26:20 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -2,14 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Category;
 import com.example.demo.repository.CategoryRepository;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
-@Tag(name = "Category CRUD")
 public class CategoryController {
 
     private final CategoryRepository repo;
@@ -26,16 +24,5 @@ public class CategoryController {
     @GetMapping
     public List<Category> getAll() {
         return repo.findAll();
-    }
-
-    @PutMapping("/{id}")
-    public Category update(@PathVariable Long id, @RequestBody Category category) {
-        category.setId(id);
-        return repo.save(category);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
-        repo.deleteById(id);
     }
 }
